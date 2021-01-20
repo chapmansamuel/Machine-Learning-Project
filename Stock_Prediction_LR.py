96dc5c580d956f7cbf4a3975037bde22c80a20c7
Samuel Chapman <71799660+chapmansamuel@users.noreply.github.com>
2021-01-19 20:02:09 -0600

    Update Stock_Prediction_LR.py

--- Stock_Prediction_LR.py
+++ Stock_Prediction_LR.py
@@ -20,22 +20,35 @@ def prepare_data(df,forecast_col,forecast_out,test_size):
     X = X[:-forecast_out] # X that will contain the training and testing
     label.dropna(inplace=True) #dropping na values
     y = np.array(label)  # assigning Y
+    Y_lately = y[:-forecast_out] #creating the y prediction value for bayesian statistics 2-d array
     X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=test_size) #cross validation
-    response = [X_train, X_test, Y_train, Y_test , X_lately]
+    response = [X_train, X_test, Y_train, Y_test , X_lately, Y_lately]
     return response
 
 def cross_val(fitment, x_train, y_train):
     crossval = cross_val_score(fitment, x_train, y_train, cv=3)
     return crossval
 
-def linear_predict(stock):
+# def f(x, stock):
+#     y = np.sqrt(x) * np.sin(x)
+#     poly = np.random.normal(0, 1, len(x))
+#     return y + stock * poly
+#
+# degree = 10
+# X = np.linspace(0, 10, 100)
+# y = f(X, noise_amount=0.1)
+# clf = linear_model.BayesianRidge()
+# clf.fit(np.vander(X, degree), y)
+
+
+def linear_predict(iterations, stock):
     df = web.DataReader(stock, 'stooq')
     df.sort_values('Date', ascending=True, inplace=True)
     data = df.filter(['Close'])
 
     forecast_col = 'Close'#choosing which column to forecast
     forecast_out = 6 #how far to forecast
-    test_size = 0.2 #the size of my test set
+    test_size = 0.3 #the size of my test set
 
     dataset = prepare_data(data,forecast_col,forecast_out,test_size) #calling the method where the cross validation and data preperation is in
 
@@ -45,8 +58,9 @@ def linear_predict(stock):
         Y_train = dataset[2]
         Y_test = dataset[3]
         X_lately = dataset[4]
+        Y_lately = dataset[5]
 
-    learner = linear_model.LinearRegression() #initializing linear regression model
+    learner = linear_model.BayesianRidge(iterations, tol=0.001, alpha_1=1e-06, alpha_2=1e-06, lambda_1=1e-06, lambda_2=1e-06, alpha_init=None, lambda_init=None, compute_score=False, fit_intercept=True, normalize=False, copy_X=True, verbose=False) #initializing linear regression model
     learner.fit(X_train,Y_train)#training the linear regression model
     score=learner.score(X_test,Y_test)#testing the linear regression model
     crossvalidated = cross_val(learner, X_train, Y_train)
@@ -60,7 +74,7 @@ def linear_predict(stock):
     st.title('{} Linear Regression Prediction.'.format(stock.upper()))
     st.line_chart(forecast)
     st.title("Model Metrics for Linear Regression.")
-    st.subheader("Test Score.")
+    st.subheader("R^2 Value.")
     st.subheader(score)
     st.subheader("Cross Validation Matrix.")
     st.subheader(crossvalidated)
